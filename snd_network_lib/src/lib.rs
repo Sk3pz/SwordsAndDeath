47fb75e65650c21c50dccbdbb6977284e97944f2
eric <skepz.dev@gmail.com>
2022-02-23 14:35:22 -0600

    make network code a shared library pt2

---
+++ snd_network_lib/src/lib.rs
@@ -0,0 +1,27 @@
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
+pub mod packet_capnp;
+
+pub mod entry_point_io;
+pub mod entry_response;
+
+pub mod login_data;
+pub mod item_data;
+pub mod loot_data;
+pub mod enemy_data;
+pub mod error_data;
+pub mod encounter_data;
+
+pub mod client_event;
+pub mod server_event;
+
+pub fn systime() -> Duration {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Fatal error occurred: System time moved backwards! Are you a time traveler?")
+}
+
+pub fn to_epoch(time: SystemTime) -> Duration {
+    time.duration_since(UNIX_EPOCH)
+        .expect("Fatal error occurred: System time moved backwards! Are you a time traveler?")
+}
