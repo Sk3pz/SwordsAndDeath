3289d3572ff8d5bf37736f03bde63ebc9e3b2b2d
DeathBySpork <ericshreve02@gmail.com>
2022-02-23 16:21:00 -0600

    start work on client

--- snd_client_rs/src/main.rs
+++ snd_client_rs/src/main.rs
@@ -16,5 +16,19 @@ fn read_input<S: Into<String>>(prompt: S) -> String {
 }
 
 fn main() {
-    let ip_pattern = Regex::new(r"^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(\.(?!$)|$)){4}$");
+    let ip_pattern = Regex::new(r"^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(\.(?!$)|$)){4}$").expect("Failed to init regex");
+    let port_pattern = Regex::new(r"^([0-9]{1,5})$").expect("Failed to init regex");
+
+    let mut ip = read_input(format!("Input the ip of the server: "));
+    let mut port = read_input(format!("Input the port of the server: "));
+
+    while !ip_pattern.is_match(ip.as_str()) {
+        eprintln!("Invalid ip! please enter a valid ip!");
+        ip = read_input(format!("Input the ip of the server: "));
+    }
+
+    while !port_pattern.is_match(port.as_str()) {
+        eprintln!("Invalid port! please enter a valid port!");
+        port = read_input(format!("Input the port of the server: "));
+    }
 }
